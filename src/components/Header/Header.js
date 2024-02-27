6c19f0367eb29aaa1c7ed30fdb225bceadbbd2fc
meekachm <meeka.chm@gmail.com>
2024-02-27 02:07:14 -0800

    Added Components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <img src="/logo.png" className="App-logo" alt="TasteTrails Logo" />
+    </div>
+  );
+}
+
+export default Header;
